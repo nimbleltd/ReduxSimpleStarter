4e5fe8cf6bd1f5559a1d988059a220b030abb56f
Paul Campbell <nimbleltd@gmail.com>
2016-03-26 22:30:50 -0500

    resolved forking issue

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+// create a new component
+// component creates HTML
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDIefq7JBKIMWiggVExyEiM08BY2CnonJQ';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+// const App = function() {
+const App = () => {
+	return (<div>
+		<SearchBar />
+	</div>
+	);
+}
+
+// put generated HTML in the DOM //
+
+ReactDOM.render(<App />, document.querySelector('.container'));
