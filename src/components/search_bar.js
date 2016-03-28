add1e342f72b58d0b86e6fe381c2c88bb1324106
Paul Campbell <nimbleltd@gmail.com>
2016-03-27 20:25:33 -0500

    test using youtube-api-search

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { term: ''};
+		this.state = { term: 'test'};
 	}
 
 	render() {
 		return (
 			<div>
-				<input onChange={event => this.setState({ term: event.target.value })} />
-				Value of the input: {this.state.term}
+				<input 
+					value = {this.state.term}
+					onChange={event => this.setState({ term: event.target.value })} />
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@
 // component creates HTML
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSerach from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDIefq7JBKIMWiggVExyEiM08BY2CnonJQ';
 
+YTSerach({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data)
+});
 
 
 // const App = function() {
