3f33f894940e4886dc21972cc1f015304ef67b3e
Paul Campbell <nimbleltd@gmail.com>
2016-03-28 06:58:21 -0500

    changing main function from const to class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 // create a new component
 // component creates HTML
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSerach from 'youtube-api-search'
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDIefq7JBKIMWiggVExyEiM08BY2CnonJQ';
+
 
 YTSerach({key: API_KEY, term: 'surfboards'}, function(data){
 	console.log(data)
@@ -14,11 +13,13 @@ YTSerach({key: API_KEY, term: 'surfboards'}, function(data){
 
 
 // const App = function() {
-const App = () => {
-	return (<div>
-		<SearchBar />
-	</div>
-	);
+class App extends Component {
+	render() {
+		return (<div>
+			<SearchBar />
+		</div>
+		);
+	}
 }
 
 // put generated HTML in the DOM //
