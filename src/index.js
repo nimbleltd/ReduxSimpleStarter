aece8a0afc485907c7585439252830d843667095
Paul Campbell <nimbleltd@gmail.com>
2016-04-01 08:37:47 -0500

    added selectedVideo variable and set it to video[0]

--- src/index.js
+++ src/index.js
@@ -10,29 +10,31 @@ const API_KEY = 'AIzaSyDIefq7JBKIMWiggVExyEiM08BY2CnonJQ';
 
 
 
-
-
-// const App = function() {
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos : [] };
+		this.state = { 
+			videos : [],
+			selectedVideo: null
+
+			 };
 
 		YTSerach({key: API_KEY, term: 'Arnold Renderer'}, (videos) =>{
 			//console.log(data)
 			//this.setState({videos:videos});
-			this.setState({ videos });
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
-
-
 	}
 
 	render() {
 		return (
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
+				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList videos={this.state.videos} />
 			</div>
 		);
